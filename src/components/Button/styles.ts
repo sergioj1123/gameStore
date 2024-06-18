00241c64512e85475e0817164b5038a8cbe12cc3
Sergio Felix <sergiofelix9@gmail.com>
2024-06-17 23:03:55 -0300

    feat: Add variant prop to reuse the component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 import colors from '../../styles/variables';
 import { Link } from 'react-router-dom';
 
-export const ButtonContainer = styled.button`
-  border: 2px solid ${colors.white};
+import { Props } from './index';
+
+export const ButtonContainer = styled.button<Props>`
+  border: 2px solid
+    ${(props) => (props.variant === 'primary' ? colors.green : colors.white)};
   color: ${colors.white};
-  background-color: transparent;
+  background-color: ${(props) =>
+    props.variant === 'primary' ? colors.green : 'transparent'};
   font-size: 16px;
   font-weight: bold;
   padding: 8px 16px;
