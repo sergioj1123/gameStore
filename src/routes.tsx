4586831e8486f080f5944cab00ab6894e8db2865
Sergio Felix <sergiofelix9@gmail.com>
2024-06-17 22:59:45 -0300

    feat: Add product route to pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
+import Product from './pages/Product';
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 );
 
